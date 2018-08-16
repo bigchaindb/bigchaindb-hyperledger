e704fc9be9a637035404009afd47916aa910b09c
jernejpregelj <jernejpregelj@Jernejs-MacBook-Pro.local>
2018-08-16 15:20:22 +0200

    bdb backend

---
+++ bdb-hyperledger-oracle/src/routes/BdbRouter.ts
@@ -0,0 +1,32 @@
+import { Router, Request, Response, NextFunction } from "express";
+
+export class BdbRouter {
+  router: Router;
+
+  /**
+   * Initialize the MeRouter
+   */
+  constructor() {
+    this.router = Router();
+    this.init();
+  }
+
+  public bdb(req: Request, res: Response, next: NextFunction) {
+    res.send({status: "success"});
+  }
+
+  /**
+   * Take each handler, and attach to one of the Express.Router's
+   * endpoints.
+   */
+  init() {
+    this.router.get("/", this.bdb);
+  }
+
+}
+
+// Create the bdbRouter, and export its configured Express.Router
+const bdbRoutes = new BdbRouter();
+bdbRoutes.init();
+
+export default bdbRoutes.router;
