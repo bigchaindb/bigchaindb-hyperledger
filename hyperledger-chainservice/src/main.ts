7a534da4eb24004c598695ee6e086e84f6229024
jernejpregelj <jernejpregelj@Jernejs-MacBook-Pro.local>
2018-08-17 16:35:10 +0200

    cors

--- hyperledger-chainservice/src/main.ts
+++ hyperledger-chainservice/src/main.ts
@@ -31,6 +31,7 @@ async function bootstrap() {
     app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
         next();
     });
 
