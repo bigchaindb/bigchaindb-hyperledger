4ad2a188331af8a58cbd54239998e435b33bd50f
Manan Patel <manan77patel@gmail.com>
2018-08-20 12:23:52 +0200

    removed websocket integration

--- ui/src/service.js
+++ ui/src/service.js
@@ -1,23 +1,3 @@
-import io from 'socket.io-client';
-
-export function wsListen(handleEvent) {
-    console.log('ws path', process.env.REACT_APP_CHAINCODE_URL)
-    
-    let socket = io.connect(process.env.REACT_APP_WS);
-
-    socket.on('connect', function(data){
-        console.log(`Server : Greetings from BDB-Hyperledger-Oracle...`)
-    });
-    
-    socket.on('data', function (data) {
-        console.log(`Server : ${data}`);
-    });
-    socket.on('disconnect', function () {
-        console.log('disconnecting')
-    });
-
-}
-
 export async function callChaincode(assetId, callback) {
 
     const rawResponse = await fetch(process.env.REACT_APP_CHAINCODE_URL, {
