9525c200b7774a1917816b9c0cd5e173d2958795
Jeroen De Prest <jeroen.deprest@hotmail.com>
2018-08-22 13:22:34 +0200

    removed unused folder

--- hyperledger-network/.circleci/clean-hlf.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-sudo rm -rf ~/HLF && mkdir -p ~/HLF
-
-# create bdb-network if not exist
-if ! docker network ls | grep bdb-network; then
-    docker network create bdb-network
-fi
--- hyperledger-network/.circleci/deploy-hlf.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# remove untagged docker images (cleanup space)
-if [[ $(docker images -f "dangling=true" -q) ]]; then
-	docker rmi $(docker images -f "dangling=true" -q)
-fi
-
-./HLF/scripts/startFabric.sh
-
