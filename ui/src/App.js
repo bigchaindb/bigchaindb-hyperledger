1e9d6ba1da52bbd6b2c4acc65572b9f8e64d25d0
Jernej Pregelj <jernej.pregelj88@gmail.com>
2018-08-23 15:26:06 +0200

    asset creation + design

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,68 +1,103 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
-import { callChaincode }  from './service.js';
-import { AppInsights } from 'applicationinsights-js';
+import {callChaincode,createNewAsset,getKeypairFromSeed} from './service.js';
+import {AppInsights} from 'applicationinsights-js';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       inputValue: '',
-      inputCallback: ''
+      inputPassphrase: '',
+      inputAsset: ''
     };
     this.handleSubmit = this.handleSubmit.bind(this);
-
+    this.handleCreate = this.handleCreate.bind(this);
     // initialize application insights
-    AppInsights.downloadAndSetup({ instrumentationKey: String(process.env.REACT_APP_APPLICATION_INSIGHTS_KEY) });
+    AppInsights.downloadAndSetup({
+      instrumentationKey: String(process.env.REACT_APP_APPLICATION_INSIGHTS_KEY)
+    });
   }
   handleValueChange(event) {
     this.setState({inputValue: event.target.value})
   }
-  handleCallbackChange(event) {
-    this.setState({inputCallback: event.target.value})
+  handlePassphraseChange(event) {
+    this.setState({inputPassphrase: event.target.value})
+  }
+  handleAssetChange(event) {
+    this.setState({inputAsset: event.target.value})
   }
   handleSubmit(event) {
-    console.log('inputCallback: ' + this.state.inputCallback);
-    console.log('inputValue: ' + this.state.inputValue);
-    AppInsights.trackEvent("UISubmit", { assetId: this.state.inputValue, callback: this.state.inputCallback });
-    callChaincode(this.state.inputValue, this.state.inputCallback);
     event.preventDefault();
+    console.log('inputValue: ' + this.state.inputValue);
+    AppInsights.trackEvent("UISubmit", {
+      assetId: this.state.inputValue
+    });
+    callChaincode(this.state.inputValue);
+  }
+  async handleCreate(event) {
+    event.preventDefault();
+    const keyPair = getKeypairFromSeed(this.state.inputPassphrase);
+    const asset = JSON.parse(this.state.inputAsset);
+    const tx = await createNewAsset(keyPair, asset, null);
+    this.setState({inputValue: tx.id});
   }
   render() {
-    return(
-      <section className="jumbotron text-center">
-        <div className="container">
-          <h1 className="jumbotron-heading">BigchainDB - Hyperledger Fabric <br /> Oracle</h1>
-          <div className="lead-body">
-            <div className="explainer">
-            This UI is for demo usage of the BigchainDB-Hyperledger Fabric oracle.
-            The following form, takes two inputs - a BigchainDB asset ID and a JavaScript function. These inputs are passed to a HyperLedger chain-code which internally passes them to the oracle. The oracle then queries BigchainDB with the asset ID and executes the callback with asset.data of the queried asset.<br />
-            The oracle then sends back the results to this UI using a websocket.
-            <br /><br />
-            In a real scenario, the chain-code can do pre-processing and create a dynamic callback before sending the request to the oracle.
+    return (<section className="jumbotron text-center">
+      <div className="container">
+        <h1>BigchainDB - Hyperledger Fabric Oracle</h1>
+        <br/><br/>
+        <div className="alert alert-warning">
+          This UI is for demo usage of the BigchainDB-Hyperledger Fabric oracle. The following form, takes two inputs - a BigchainDB asset ID and a JavaScript function. These inputs are passed to a HyperLedger chain-code which internally passes them to the oracle. The oracle then queries BigchainDB with the asset ID and executes the callback with asset.data of the queried asset.<br/>
+          The oracle then sends back the results to this UI using a websocket.
+          <br/><br/>
+          In a real scenario, the chain-code can do pre-processing and create a dynamic callback before sending the request to the oracle.
+        </div>
+        <br/><br/>
+        <div className="row">
+          <div className="col-6">
+            <h4 className="jumbotron-heading">BigchainDB asset creation</h4>
+            <br/>
+            <div className="lead-body">
+              <form className="inputForm" onSubmit={this.handleCreate}>
+                <div>
+                  <label htmlFor="inputPassphrase" className="sr-only">Passphrase:</label>
+                  <input type="text" name="inputPassphrase" id="inputPassphrase" className="form-control" placeholder="Passphrase" value={this.state.inputPassphrase} onChange={this.handlePassphraseChange.bind(this)}/>
+                </div>
+                <br/>
+                <div>
+                  <label htmlFor="inputAsset" className="sr-only">Asset:</label>
+                  <textarea type="text" name="inputAsset" id="inputAsset" className="form-control" placeholder="JSON Asset data" value={this.state.inputAsset} onChange={this.handleAssetChange.bind(this)}></textarea>
+                </div>
+                <br/>
+                <div>
+                  <button className="btn btn-lg btn-primary btn-block" type="submit">Create asset</button>
+                </div>
+              </form>
             </div>
-            <form className="inputForm" onSubmit={this.handleSubmit}>
-              <div>
-                <label htmlFor="inputValue" className="sr-only">Value:</label>
-                <input type="text" name="inputValue" id="inputValue" className="form-control" placeholder="Value" value={this.state.inputValue} onChange={this.handleValueChange.bind(this)}/>
-              </div>
-              <br/>
-              <div>
-                <label htmlFor="inputCallback" className="sr-only">Callback:</label>
-                <textarea type="text" name="inputCallback" id="inputCallback" className="form-control" placeholder="Code callback" value={this.state.inputCallback} onChange={this.handleCallbackChange.bind(this)}></textarea>
-              </div>
-              <br/><br/>
-              <div>
-                <button className="btn btn-lg btn-primary btn-block" type="submit">Submit</button>
-              </div>
-            </form>
-            <div className="footer">
-              Developed By: <a href="https://www.bigchaindb.com">BigchainDB</a> and <a href="https://theledger.be">TheLedger</a>
+          </div>
+          <div className="col-6">
+            <h4 className="jumbotron-heading">Hyperledger Fabric Oracle</h4>
+            <br/>
+            <div className="lead-body">
+              <form className="inputForm" onSubmit={this.handleSubmit}>
+                <div>
+                  <label htmlFor="inputValue" className="sr-only">Value:</label>
+                  <input type="text" name="inputValue" id="inputValue" className="form-control" placeholder="Asset id" value={this.state.inputValue} onChange={this.handleValueChange.bind(this)}/>
+                </div>
+                <br/>
+                <div>
+                  <button className="btn btn-lg btn-primary btn-block" type="submit">Send to Hyperledger</button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
-      </section>
-    )
+        <div className="footer">
+          Developed By: <a href="https://www.bigchaindb.com">BigchainDB</a> and <a href="https://theledger.be">TheLedger</a>
+        </div>
+      </div>
+    </section>)
   }
 }
 
--- ui/src/service.js
+++ ui/src/service.js
@@ -1,3 +1,11 @@
+import * as driver from 'bigchaindb-driver';
+import * as bip39 from 'bip39';
+
+export const BDB_SERVER_URL = process.env.REACT_APP_BDB_SERVER_URL || 'http://localhost:9984';
+export const BDB_API_PATH = `${BDB_SERVER_URL}/api/v1/`;
+
+let conn;
+
 export async function callChaincode(assetId, callback) {
     const rawResponse = await fetch(process.env.REACT_APP_CHAINCODE_URL, {
         method: 'POST',
@@ -12,7 +20,44 @@ export async function callChaincode(assetId, callback) {
     });
 
     const content = await rawResponse.json();
-    
+
     console.log(content);
     return true
+}
+
+// gets a Ed25519Keypair from a pass phrase
+export const getKeypairFromSeed = (seed) => {
+    return new driver.Ed25519Keypair(bip39.mnemonicToSeed(seed).slice(0, 32));
+};
+
+// Creates a new asset in BigchainDB
+export async function createNewAsset(keypair, asset, metadata) {
+    await _getConnection();
+    const condition = driver.Transaction.makeEd25519Condition(keypair.publicKey, true);
+
+    const output = driver.Transaction.makeOutput(condition);
+    output.public_keys = [keypair.publicKey];
+
+    const transaction = driver.Transaction.makeCreateTransaction(
+        asset,
+        metadata,
+        [output],
+        keypair.publicKey
+    );
+
+    const txSigned = driver.Transaction.signTransaction(transaction, keypair.privateKey);
+    return await conn.postTransactionCommit(txSigned)
+        .then(retrievedTx => {
+            return retrievedTx
+        })
+        .catch(err => {
+            return false
+        });
+}
+
+// private: creates a connection to BDB server
+export async function _getConnection() {
+    if (!conn) {
+        conn = new driver.Connection(BDB_API_PATH);
+    }
 }
